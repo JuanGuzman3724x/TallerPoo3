db9d7bc6bc3f5a2aa6f2eb504b80fa9745e91277
juan3724x <juan3724x@gmail.com>
2025-11-13 12:45:28 -0300

    primera parte de creacion
    cree algunas clases

---
+++ src/clases/Usuario.java
@@ -0,0 +1,23 @@
+package clases;
+
+public class Usuario {
+	private String user;
+	private String password;
+	private String  rol;
+	public Usuario(String user, String password, String rol) {
+		
+		this.user = user;
+		this.password = password;
+		this.rol = rol;
+	}
+	public String getUser() {
+		return user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getRol() {
+		return rol;
+	}
+	
+}
---
+++ src/patrones/Visitor.java
@@ -0,0 +1,6 @@
+package patrones;
+
+public interface Visitor {
+	
+
+}
