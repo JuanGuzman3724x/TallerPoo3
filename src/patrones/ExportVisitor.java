b56f97649ddf639d947aa00fa31c36c56719f20c
juan3724x <juan3724x@gmail.com>
2025-11-17 22:36:39 -0300

    1.1.17.1
    olvide guardar esto en el commit anterior

--- src/patrones/ExportVisitor.java
+++ src/patrones/ExportVisitor.java
@@ -1,5 +1,27 @@
 package patrones;
 
-public class ExportVisitor {
+import clases.TareaBug;
+import clases.TareaDocumentacion;
+import clases.TareaFeature;
+
+public class ExportVisitor implements Visitor {
+
+	@Override
+	public void aceptar(TareaDocumentacion documento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aceptar(TareaFeature feature) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aceptar(TareaBug bug) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
